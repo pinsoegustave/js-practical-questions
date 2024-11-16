fb0dd680d61476ca80db2475f6e5188b746f4917
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-11-16 22:48:25 +0200

    ForEach case

---
+++ Udacity/forEach.js
@@ -0,0 +1,7 @@
+var donuts = ["jelly donut", "chocolate donut", "glazed donut"];
+
+for (var i = 0; i < donuts.length; i++) {
+    donuts[i] += " hole";
+    donuts[i] = donuts[i].toUpperCase();
+    console.log(donuts[i]);
+  }

cfb92c25c2a76c0c2751ad3a38fd98f4b3bb6f08
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-11-19 11:31:53 +0200

    codewars: Distinct function

--- codewars-katas/distinct.js
+++ codewars-katas/distinct.js
@@ -1,7 +1,9 @@
 function distinct(a) {
-    const result = new Set(a);
+    const result = []
+    const b = new Set(a);
+    result.push(...b);
     
-    // result.add(a);
-    return result.toString();
+    // result.add(b);
+    return result;
 }
-console.log(distinct([1,1,4,5,4,3,2,2,3,4,2]));
+console.log(distinct([1,-1,4,5,4,3,2,2,3,4,2]));

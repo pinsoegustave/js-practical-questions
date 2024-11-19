d45dc589cae5b4ba7d5591874509e4a567c8e523
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-11-19 20:09:22 +0200

    String method: includes

---
+++ string-methods/including.js
@@ -0,0 +1,4 @@
+const name = "Gustave" 
+const sentence = "How to Create an Engineer Portfolio Your First Junior Dev Job"
+console.log(name.includes("t", 5));
+console.log(sentence.includes("an Engineer"));

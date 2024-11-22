fc2288334ab15448973b1a644b2053243e128a27
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-11-22 11:12:40 +0200

    codewars: score return

--- codewars-katas/teamPoints.js
+++ codewars-katas/teamPoints.js
@@ -1,21 +1,19 @@
 function points(games) {
-    const num = games.length;
-    let count = ''
+        scores = 0;
+        for (let game of games) {
 
-    let point = games.forEach(element => {
-        let scores = element.split('');
-
-        console.log(scores[0].length);
-
-        if (scores[0] == scores[2]) {
-            // let final = scores[0].length;
-            // console.log("final");
+            let [ a, b] = game.split(":").map(Number);
+            scores =+ a > b  ? 3 : (a == b) ? 1 : 0;
+            
+            if (a > b) {
+                scores +=3;
+            }
+            else if (a == b) {
+                scores++;
+            }
         }
-    });
-    // console.log(point)
+        
+        return scores;
 
-    // if (games[1][0] = games[1][1]) {
-    //     count++;
-    // }
 }
-points(["1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"]) // should return 10)
+console.log(points(["1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"])) // should return 10)

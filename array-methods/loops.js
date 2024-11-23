648fe2762330ceed6ec6e35431ed89b4e01b0212
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-11-23 19:11:57 +0200

    Array Iteration: forEach

---
+++ array-methods/loops.js
@@ -0,0 +1,6 @@
+const colors = ["red", "blue", "yellow", ]
+
+//forEach: executes a provided function once for each array element
+colors.forEach((element) => {
+    console.log(element);  // returns red, blue, yellow
+})

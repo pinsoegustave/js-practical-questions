d32d7327fe2f2faf05fb24c8ab0983a832c1d7e9
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-02 16:05:39 +0200

    Codewars: Binary to Decimal number

---
+++ codewars-katas/binaryArray.js
@@ -0,0 +1,10 @@
+const binaryArrayToNumber = arr => {
+    // your code
+    // const num = arr.reverse().join('');
+    // console.log(num);
+  
+    // return parseInt(num, 2);
+
+    return arr.reduce((a, b) => a << 1 | b);
+};
+console.log(binaryArrayToNumber([1, 1, 1, 1]));

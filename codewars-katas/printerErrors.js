a744f75c33773fa5bb1f1dfe94b0d03d4b8e8e0d
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-05 21:34:57 +0200

    Codewars: Printer Errors

---
+++ codewars-katas/printerErrors.js
@@ -0,0 +1,9 @@
+function printerError(s) {
+    const validColors = 'abcdefghijklm'
+
+    const errors = Array.from(s).filter(char => !validColors.includes(char)).length;
+    
+    return `${errors}/${s.length}`
+}
+console.log(printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"));
+// printerError();

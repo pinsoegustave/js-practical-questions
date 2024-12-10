511d4c5e0b1ec158d16ee1d48a6ca3450728fe00
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-10 11:14:24 +0200

    codewars: refactoring

--- codewars-katas/splitInParts.js
+++ codewars-katas/splitInParts.js
@@ -2,7 +2,7 @@ var splitInParts = function(s, partLength){
     // Good Luck!
     let chunks = []
   
-    for (let a = 0; a < s.length; a+=partLength) {
+    for (let a = 0; a <= s.length; a+=partLength) {
       chunks.push(s.slice(a, a + partLength))
     }
     
@@ -10,4 +10,4 @@ var splitInParts = function(s, partLength){
 }
 
 console.log(splitInParts('supercalifragilisticexpialidocious', 3));
-console.log(splitInParts('HelloKata', 1));
+console.log(splitInParts('HelloKata', 3));

9803f010fd745afaf97838c5fb0e3c3d62368baa
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-12 13:45:23 +0200

    JS-DOM: Get a form valuet

---
+++ JS-DOM/form.js
@@ -0,0 +1,17 @@
+const list = document.querySelector('#book-list');
+
+list.addEventListener('click', function(e){
+    if (e.target.className === "delete") {
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
+});
+
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
+})
+

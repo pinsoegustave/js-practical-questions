9a1e0e7c455e2dd3569ec4c5e783e87d717b2933
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-13 10:43:47 +0200

    TODO-APP

--- JS-DOM/form.js
+++ JS-DOM/form.js
@@ -1,3 +1,18 @@
+const myList = document.getElementById('myList');
+const docFrag = document.createDocumentFragment();
+
+
+let item1 = document.createElement('li');
+let item2 = document.createElement('li');
+
+item1.textContent = "Apples"
+item2.textContent = "Bananas"
+
+docFrag.appendChild(item1);
+docFrag.appendChild(item2);
+
+myList.appendChild(docFrag);
+
 const list = document.querySelector('#book-list');
 
 list.addEventListener('click', function(e){

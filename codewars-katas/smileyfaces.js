b299bc2f695d7b62adfc0e3664c9ec1b2f0eef34
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-13 13:22:09 +0200

    codewars: complete smileys function

--- codewars-katas/smileyfaces.js
+++ codewars-katas/smileyfaces.js
@@ -1,23 +1,14 @@
 //return the total number of smiling faces in the array
 function countSmileys(arr) {
-    const tester = [":",";","-","~",")","D"]
-    
-    let count = 0;
+    const tester = /^[:;][-~]?[)D]$/
 
-    // for (let index = 0; index < arr.length; index++) {   
-    //     if (!arr.includes(tester)) {
-    //         // count = count + 1;
-    //         console.log(index);
-    //     }
-    //     else {
-    //         console.log("Array has invalid smiles");
-    //     }
-    // }
-    const checker = arr.filter(ele => !ele.includes(tester)).length
-    console.log(checker)
+    const checker = arr.filter(ele => tester.test(ele)).length
+
+
+    console.log(checker);
 }
 
-countSmileys([':D',':~)',';~D',':)']);
+countSmileys([':D',':~)',';~D',':)',';*']);
 // countSmileys([]);
 // countSmileys([':D',':~)',';~D',':)',':D',':~)',';~D',':)']);
-// countSmileys([';]', ':[', ';*', ':$', ';-D']);  // 1
+countSmileys([':]', ';[', ':*', ';$',':D']);  // 1

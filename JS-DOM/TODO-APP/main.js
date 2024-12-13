b3772b231126ba6b7185cd35773a24e6a7ab0a3c
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-13 15:18:00 +0200

    JS-DOM Exercise: ToDo App

--- JS-DOM/TODO-APP/main.js
+++ JS-DOM/TODO-APP/main.js
@@ -1,5 +1,4 @@
 const ulTasks = document.querySelector('#tasks');
-const addForm = document.querySelector('#add-form');
 const delBtn = document.querySelector('.delete');
 
 
@@ -8,4 +7,27 @@ ulTasks.addEventListener('click', (e) => {
         const li = e.target.parentElement;
         ulTasks.removeChild(li)
     };
-})
+});
+
+const addForm = document.forms['add-form'];
+
+addForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const value = addForm.querySelector('input[type="text"]').value
+    console.log(value);
+
+    const newLi = document.createElement('li');
+    const newMark = document.createElement('span');
+    const newElement = document.createElement('span');
+    const delElement = document.createElement('span');
+    newMark.textContent = "Mark Completed"
+    newElement.textContent = value;
+    delElement.textContent = "Delete"
+
+    newLi.appendChild(newMark);
+    newLi.appendChild(newElement);
+    newLi.appendChild(delElement);
+
+    ulTasks.appendChild(newLi);
+});

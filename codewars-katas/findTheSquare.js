c2a8d4e933e4e6cae4d6af7a47417572b4d3699b
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-17 15:06:58 +0200

    Codewars: Find the Squares

---
+++ codewars-katas/findTheSquare.js
@@ -0,0 +1,15 @@
+const findTheSquares = num => {
+    if (num % 2 === 0) {
+        return "Number should be an Odd"
+    }
+    let squares = []
+    for (let i = 0; i < num; i++) {
+        squares.push(i*i);
+    }
+    for (let a = 0; a < squares.length - 1; a++) {
+        if(squares[a+1] - squares[a] === num) {
+            return String(squares[a+1] + "-" + squares[a]);
+        }
+    }
+}
+console.log(findTheSquares(9));

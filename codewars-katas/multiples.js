deab5d4b95e0e475e8a1ce44972031bc44522b12
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-23 21:36:37 +0200

    Codewars: Refactor get multiples

--- codewars-katas/multiples.js
+++ codewars-katas/multiples.js
@@ -1,11 +1,19 @@
 function getMultiples (number) {
     let multiples = [];
-
     if (number < 0) return 0
-    for (let a = 1; a <= number; a++) {
+
+    for (let a = 1; a < number; a++) {
         multiples.push(a);
     }
-    return multiples.map((_, ele) => ele % 3===0);
+    return multiples.map(ele => {
+        if(ele % 3 === 0 || ele % 5 === 0) {
+            return ele;
+        } else {
+            return null;
+        }
+    }).filter(num => num !== null).reduce((acc, curr) => acc + curr, 0);
+
+    
     
 }
 console.log(getMultiples(10))

587ac2e479a1fc351e356b70c72e0065c4fbdddb
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-24 10:22:37 +0200

    JS-Date: create date object 4

--- date-time/main.js
+++ date-time/main.js
@@ -18,4 +18,17 @@ const date4 = new Date("2020");
 //passing also the specific time to ISO dates
 const date5 = new Date("1973-03-03T09:46:40.000Z");
 console.log(date5); // returns the actual passed date
- 
+
+
+//creating a new Date object by passing specific date and time
+const time2 = new Date(Date.UTC(2020, 1, 20, 4, 12, 11, 0)); //this has no effect with daylight saving time
+const time3 = new Date(2020, 1, 20, 4, 12, 11, 0); // it is sometimes affected with no time zone adjustments(like daylight saving time) where the hours
+console.log(time2);
+
+// if four numbers are passed, it represents year, month, day and hours. i.e,
+const time4 = new Date(Date.UTC(2018, 3, 11, 4));
+console.log(time4);
+
+//similary, if two arguments are passed, it represents year and month
+const time5 = new Date(2020, 1);
+console.log(time5);

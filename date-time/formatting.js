f43b7969582a80768285f7f13ce363aa6e0349ba
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-25 16:32:31 +0200

    codewars kata refactor

--- date-time/formatting.js
+++ date-time/formatting.js
@@ -3,6 +3,9 @@ const currentDate = new Date();
 const date = currentDate.getDate();
 const month = currentDate.getMonth();
 const year  =currentDate.getFullYear();
+const year1  =currentDate.getFullYear();
+const year2  =currentDate.getFullYear();
+const year3  =currentDate.getFullYear();
 
 let monthDateYear = (month+1) + '/' + date + '/' + year;
 

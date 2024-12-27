e3244e2acd6c6896ea0873f9c0360a7fd56c8e8c
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-27 16:15:07 +0200

    Objects: getOwnPropertyName

---
+++ Objects/getOwnPropertyName.js
@@ -0,0 +1,9 @@
+// getOwnPropertyNames: returns an array of all properties' names
+const obj1 = {
+    name: "Gustave",
+    class: "AUCA2",
+    age: 12,
+    value: "$200M"
+}
+
+console.log(Object.getOwnPropertyNames(obj1));

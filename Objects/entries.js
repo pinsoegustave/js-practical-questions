397c7996ca55d9f228e598ca22106819d38469cb
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-27 14:37:28 +0200

    Objects: entries applications

--- Objects/entries.js
+++ Objects/entries.js
@@ -10,7 +10,7 @@ const object1 = {
     a: 'something',
     b: 42
 };
-
+// Iterating through objects
 for (const [key, value] of Object.entries(object1)) {
     console.log(`${key}: ${value}`)
 }
@@ -19,5 +19,16 @@ const arrayLike = {
     0: "a", 1: "b", 3: "c"
 }
 const oneArray = Object.entries(arrayLike);
-const solution = oneArray.flat(Infinity);
+// Get one array of all key and values from an object
+const solution = oneArray.flat(Infinity); 
 console.log(solution);
+
+//used on primitives
+console.log(Object.entries("foobar")); //Strings have indices as enumerable own properties
+
+console.log(Object.entries(100)); //returns [] // Only strings have enumerable properties, while other primitives return an empty array.
+
+//converting am object to a Map
+const obj1 = { foo: "bar", baz: 42 }
+const map = new Map(Object.entries(obj1));
+console.log(map); // returns Map(2) { 'foo' => 'bar', 'baz' => 42 }

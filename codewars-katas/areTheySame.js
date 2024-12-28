b0433b96a6351cfb26844ec28be1e1688b42ef9c
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-28 20:40:26 +0200

    Codewars: 6kyu(are they the same)

---
+++ codewars-katas/areTheySame.js
@@ -0,0 +1,15 @@
+function comp(array1, array2){
+    //your code here
+    if (array1 === null || array2 === null) return false;
+    // if (array1 || array2 !== null) return false;
+
+    let array3 = array1.map((ele) => ele*ele).sort((a, b) => a - b);
+    let sortedArr4 = array2.sort((a, b) => a - b);
+
+    let result = array3.length === sortedArr4.length &&
+    array3.every((ele, index) => ele === sortedArr4[index]);
+
+
+    return result;
+}
+console.log(comp([4, 4], [1, 31]));

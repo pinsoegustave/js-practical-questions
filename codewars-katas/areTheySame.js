02568c1191088cfcf135efe5f0a951e8d9f8ac8b
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-28 20:40:57 +0200

    refactor before

--- codewars-katas/areTheySame.js
+++ codewars-katas/areTheySame.js
@@ -1,7 +1,6 @@
 function comp(array1, array2){
     //your code here
     if (array1 === null || array2 === null) return false;
-    // if (array1 || array2 !== null) return false;
 
     let array3 = array1.map((ele) => ele*ele).sort((a, b) => a - b);
     let sortedArr4 = array2.sort((a, b) => a - b);

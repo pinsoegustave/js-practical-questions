c669f500cfc40667148d45280cfac66b9be01949
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-29 15:16:03 +0200

    this keyword: use case in constructor calls

---
+++ thisKeyword/constructor.js
@@ -0,0 +1,13 @@
+
+function Person(firstName, lastName) {
+    // console.log(this);
+    this.firstName = firstName;
+    // console.log(this);
+    this.lastName = lastName;
+    // console.log(this);
+
+    return this;
+}
+
+const person = new Person("Jane", "Peter");
+console.log(person);

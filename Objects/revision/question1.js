7b4ee537a879b65a3eb6e7ee43e6bf5e972f259e
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-29 16:35:03 +0200

    objects revision: Question 1

---
+++ Objects/revision/question1.js
@@ -0,0 +1,8 @@
+// Create an object named person with the following properties: name (string), age (number), and isStudent (boolean). How would you access the name property?
+const person = {
+    name: "Gandhi",
+    age: 65,
+    isStudent: false
+}
+
+console.log(person.name);

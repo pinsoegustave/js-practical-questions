7727b9076d0d180d09b8fd2fec49ce2ba3011a6c
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-12-30 11:31:24 +0200

    Codewars: Sum without max and min

--- codewars-katas/sumWithout.js
+++ codewars-katas/sumWithout.js
@@ -1,7 +1,31 @@
 // Function to get the sum of array numbers
-
 function sumArray(array) {
-    console.log(Math.max(array));
-}
+    if (!array || array.length <= 2) return 0;
+  
+  const maxNum = Math.max(...array);
+  const minNum = Math.min(...array);
+  
+  let minRemoved = false
+  let maxRemoved = false
+  
+  const digArr = array.filter(digit => {
+    if (digit === maxNum && !maxRemoved) {
+        maxRemoved = true;
+        return false;
+    }
+    if (digit === minNum && !minRemoved) {
+        minRemoved = true;
+        return false;
+    }
+    return true;
+});
 
-sumArray([6, 2, 1, 8, 10])
+  const solution = digArr.reduce((acc, num) => acc + num, 0)
+  
+  return solution;
+}
+console.log(sumArray([6, 2, 1, 8, 10]));
+console.log(sumArray([]));
+console.log(sumArray([3]));
+console.log(sumArray([3, 5]));
+console.log(sumArray([ 0, 1, 6, 10, 10 ]));

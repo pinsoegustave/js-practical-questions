8c1c0bb1b38d450e07dcb3692edff1eb8cf66357
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-01-04 20:39:52 +0200

    Codewars: Kata Revision

--- codewars-katas/checkYourPoints.js
+++ codewars-katas/checkYourPoints.js
@@ -1,6 +1,8 @@
 function testMarks(classAverage, yourPoints) {
     const score = classAverage.reduce((num, currentV) => num + currentV, 0)/ classAverage.length;
     return yourPoints > score;
+
+    ffsdf
 }
 
 testMarks([25, 42]);

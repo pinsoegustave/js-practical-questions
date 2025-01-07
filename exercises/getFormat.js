ad07ea0b09f78f9dfa287cfc47d1970bf36c1556
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-01-07 21:17:31 +0200

    JS Exercises: Round 2

---
+++ exercises/getFormat.js
@@ -0,0 +1,6 @@
+// Format any integer provided into a string with "," (commas) in the correct places.
+
+function getFormat(num) {
+    return num.toLocaleString();
+}
+console.log(getFormat(13489579834));

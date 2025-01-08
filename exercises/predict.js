a88c14081dd1a8f4c837cf44a9693a627cfee60f
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-01-08 10:33:55 +0200

    JS Revision: Round 9 and 10

--- exercises/predict.js
+++ exercises/predict.js
@@ -1,3 +1,10 @@
-let arr = [1,2,3,4]
-delete arr[1]
-console.log(arr)
+// let arr = [1,2,3,4]
+// delete arr[1]
+// console.log(arr)
+
+function test() {
+    console.log(a);
+    var a = 10;
+    console.log(a);
+}
+test();

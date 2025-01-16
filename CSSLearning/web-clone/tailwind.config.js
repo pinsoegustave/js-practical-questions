1e331334ec5a218b63584485ccda6c73568839bd
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-01-16 16:12:16 +0200

    CSS Exercises: News section

--- CSSLearning/web-clone/tailwind.config.js
+++ CSSLearning/web-clone/tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '400px',
+        '3xl': '1680px',
+        '4xl': '2200px'
+      }
+    },
   },
   plugins: [],
 }

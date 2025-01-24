7a381fd93239e410c1242f5de4c7883482b9a005
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-01-24 14:21:48 +0200

    es6: Iterator

---
+++ es6/iterator.js
@@ -0,0 +1,7 @@
+const digits = [0, 1  ]
+
+const arrayIterator = digits[Symbol.iterator]();
+
+console.log(arrayIterator.next());
+console.log(arrayIterator.next());
+console.log(arrayIterator.next());

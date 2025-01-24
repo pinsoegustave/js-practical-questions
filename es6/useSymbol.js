2e87a646fde88de37dd96dd02fedaf580438ffec
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-01-24 14:18:28 +0200

    es6: Symbol example

---
+++ es6/useSymbol.js
@@ -0,0 +1,8 @@
+const bowl = {
+    [Symbol('apple')]: { color: "red", weight: "135,873" },
+    [Symbol('orange')]: { color: "orange", weight: "154" },
+    [Symbol('banana')]: { color: "banana", weight: "179" },
+    [Symbol('apple')]: { color: "reddish", weight: "173.3" }
+}
+
+console.log(bowl);

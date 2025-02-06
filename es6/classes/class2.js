864f62975043304d014a69fe5267e4bab425b16d
Gym Ituze <ntwalimudasubirag@gmail.com>
2025-02-06 10:13:22 +0200

    ES6: Class 1 example

--- es6/classes/class2.js
+++ es6/classes/class2.js
@@ -1,24 +1,47 @@
 // getters and setters in normal languages
 
+// class Person {
+//     constructor(name) {
+//         this.setName(name);
+//     }
+//     getName() {
+//         return this.name;
+//     }
+//     setName(newName) {
+//         newName = newName.trim();
+
+//         if (newName === ' ') {
+//             console.error('The name cannot be empty');
+//         }
+//         this.name = newName;
+//     }
+// }
+
+// let person = new Person('Jane Paul');
+// console.log(person);
+
+// person.setName('Jane Smith');
+// console.log(person.getName());
+
 class Person {
     constructor(name) {
-        this.setName(name);
+        this._name = name;
     }
-    getName() {
-        return this.name;
-    }
-    setName(newName) {
-        newName = newName.trim();
 
-        if (newName === ' ') {
-            console.error('The name cannot be empty');
-        }
-        this.name = newName;
+    get name() {
+        return this._name;
     }
+
+    // set name(newName) {
+    //     newName = newName.trim();
+    //     if (newName === '') {
+    //         throw new Error("The name cannot be empty!");
+    //     }
+    //     this._name = newName;
+    // }
 }
+let person = new Person("Wamuuuzolo bodo");
+console.log(person.name);
 
-let person = new Person('Jane Paul');
-console.log(person);
-
-person.setName('Jane Smith');
-console.log(person.getName());
+person.name = 'Jane Smith';
+console.log(person.name); // Jane Doe

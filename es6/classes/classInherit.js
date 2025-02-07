1b23b7c58658d1a03433e57a9877b50800286164
Gym Ituze <ntwalimudasubirag@gmail.com>
2025-02-07 13:07:37 +0200

    ES6: inheritance with classes

---
+++ es6/classes/classInherit.js
@@ -0,0 +1,23 @@
+class Animal {
+    constructor(legs) {
+        this.legs = legs;
+    }
+
+    walk() {
+        console.log('walking on '+ this.legs +' legs');
+    }
+}
+
+class Birds extends Animal {
+    constructor(legs) {
+        super(legs);
+    }
+
+    fly() {
+        console.log('flying');
+    }
+}
+
+let birdPigeon = new Birds(2);
+birdPigeon.walk();
+birdPigeon.fly();

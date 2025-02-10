c259d9186c17027fb546289cbb9fd17e6d155860
Gym Ituze <ntwalimudasubirag@gmail.com>
2025-02-10 11:39:44 +0200

    ES6 Classes: metaproperty on constructors

--- es6/classes/metaproperty.js
+++ es6/classes/metaproperty.js
@@ -1,13 +1,35 @@
-
-function Person(name) {
-    if ( !new.target) {
-        throw "must have a new operator!"
+// new.target on Constructors
+class Person {
+    constructor(name) {
+        this.name = name;
+        console.log(new.target.name);
     }
-    this.name = name;
 }
 
-let john = new Person('John');
-console.log(john.name);
+class Employee extends Person {
+    constructor(name, title) {
+        super(name);
+        this.title = title;
+    }
+}
 
-Person('Lily');
-console.log(window.name);
+let john = new Person("John Doe");
+let lily = new Employee("Lily Bush", "Employee");
+
+// console.log(john);
+
+
+
+// new.target on Regular functions
+// function Person(name) {
+//     if ( !new.target) {
+//         throw "must have a new operator!"
+//     }
+//     this.name = name;
+// }
+
+// let john = new Person('John');
+// console.log(john.name);
+
+// Person('Lily');
+// console.log(window.name);

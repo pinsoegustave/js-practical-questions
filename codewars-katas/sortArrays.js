c8eeb0b492729d54c73e1da7e65472e3255038de
Gym Ituze <ntwalimudasubirag@gmail.com>
2025-02-12 13:53:59 +0200

    Codewars: complete sortArrays

--- codewars-katas/sortArrays.js
+++ codewars-katas/sortArrays.js
@@ -1,6 +1,9 @@
 const sortme = (names) => {
     // your code here
-    
-    return names; 
+    return names.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase())); 
 }
-  
+console.log(sortme(["do", "Big", "Do","Comb", "Made", "big"]));
+// console.log(sortme(["Hello","there","I'm","fine"]));
+// console.log(sortme(["C", "d", "a", "B"]));
+// console.log(sortme(["at","big","big","child","come","do","Do","Group","Her","life","New","same","this","try","woman"]));
+// 'at', 'big', 'big', 'child', 'come', 'Do', 'do', 'Group', 'Her', 'life', 'New', 'same', 'this', 'try', 'woman'

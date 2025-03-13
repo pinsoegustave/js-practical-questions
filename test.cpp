e5ac01358d7f602411a67b45bfab4236f6783c6a
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-03-13 12:50:01 +0200

    c program

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void printWordWithStars(const char *word) {
+    while (*word) {
+        printf("%c*", *word);
+        word++;
+    }
+    printf("\n");
+}
+
+int main() {
+    char word[100];
+    
+    // printf("Enter a word: ");
+    // scanf("%s", word);
+    
+    // printf("Output: ");
+    printWordWithStars("KIGALI");
+    
+    return 0;
+}

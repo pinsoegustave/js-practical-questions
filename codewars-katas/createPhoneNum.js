42c2e7cc88044a2ab65ff6d262cfab2df77a43c1
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-03-15 19:52:10 +0200

    codewars: added splice method

--- codewars-katas/createPhoneNum.js
+++ codewars-katas/createPhoneNum.js
@@ -1,4 +1,4 @@
 function createPhoneNumber(numbers){
-    return numbers.join('').splice()
+    return numbers.join('').splice(1, 3)
 }
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));   // return (123) 456-7890

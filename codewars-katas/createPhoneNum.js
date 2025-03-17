c00039fd547dd803613cdcae99f8cbca57a1c742
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-03-17 17:17:51 +0200

    Codewars: complete createPhone

--- codewars-katas/createPhoneNum.js
+++ codewars-katas/createPhoneNum.js
@@ -1,4 +1,5 @@
 function createPhoneNumber(numbers){
-    return numbers.join('').splice(1, 3).join('')
+    return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
 }
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));   // return (123) 456-7890
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));   
+// return (123) 456-7890

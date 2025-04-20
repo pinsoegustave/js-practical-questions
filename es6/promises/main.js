5778ea6140fe10c1f07c012e52433d407c6ca08a
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-04-20 11:45:59 +0200

    async: icecream business

--- es6/promises/main.js
+++ es6/promises/main.js
@@ -1,5 +1,51 @@
-function syncfunc() {
-    const answer = "my friend"
-    return 'hello world, ' + answer;
+
+let stocks = {
+    Fruits: ["strawberry", "grapes", "banana", "apple"],
+    liquid: ["water", "ice"],
+    holder: ["cone", "cup", "stick"],
+    toppings: ["chocolate", "peanuts"]
 }
-console.log(syncfunc());
+
+
+let order = (fruitname, call_production) => {
+
+    setTimeout(() => {
+        console.log(`${stocks.Fruits[fruitname]} was selected`);
+
+        call_production();
+    }, 2000)
+};
+
+
+let production = () => {
+
+    setTimeout(() => {
+        console.log("Production has started");
+
+        setTimeout(() => {
+            console.log("the fruit has been chopped");
+
+            setTimeout(() => {
+                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`)
+
+                setTimeout(() => {
+                    console.log("the machine was started");
+                    
+                    setTimeout(() => {
+                        console.log(`ice cream was placed on ${stocks.holder[0]}`);
+
+                        setTimeout(() => {
+                            console.log(`${stocks.toppings[0]} was added as toppings`);
+
+                            setTimeout(() => {
+                                console.log("You can serve the ice cream");
+                            }, 2000)
+                        }, 3000);
+                    }, 2000);
+                }, 1000);
+            }, 1000);
+        }, 2000);
+    }, 0);
+}; 
+
+order(0,production);

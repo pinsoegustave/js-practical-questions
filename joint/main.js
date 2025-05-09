07e24188f2564936496b47f05e562ea499ce5374
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-05-09 11:11:58 +0200

    present1

---
+++ joint/main.js
@@ -0,0 +1,19 @@
+let x = 10;
+let y = 5;
+
+// Arithmetic
+console.log(x + y); // 15
+
+// Comparison
+console.log(x > y); // true
+
+// Logical
+console.log(x > 5 && y < 10); // true
+
+// Ternary
+let result = x > y ? "X is larger" : "Y is larger";
+
+
+
+
+

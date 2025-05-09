0b051647cdf5ded8b992dc851077982419c2cf9b
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-05-09 11:12:15 +0200

    present2

--- joint/main.js
+++ joint/main.js
@@ -13,6 +13,15 @@ console.log(x > 5 && y < 10); // true
 // Ternary
 let result = x > y ? "X is larger" : "Y is larger";
 
+// Nullish Coalescing
+let userInput = null;
+let defaultVal = userInput ?? "Default"; // "Default"
+
+// Optional Chaining
+let user = { profile: { name: "Alex" } };
+console.log(user.profile?.name); // "Alex"
+console.log(user.address?.city); // undefined
+
 
 
 

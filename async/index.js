5f715a2e3f8499fbaf15f0a93326c613d826b34f
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-05-17 23:35:21 +0200

    async: prom1 test

--- async/index.js
+++ async/index.js
@@ -8,5 +8,20 @@ let stocks = {
 let is_shop_open = true;
 
 let order = (time, work) => {
+    return new Promise((resolve, reject) => {
 
+        if (is_shop_open) {
+            
+            setTimeout(() => {
+                resolve(work())
+            }, time)
+            
+
+        }
+        else {
+            reject(console.log("Our shop is closed"))
+        }
+    })
 }
+
+order(2000, ()=>{ console.log(`${stocks.Fruits[0]}`) } )

3338ad9de44b0c7f44c840ebc69863eb0f3d6e31
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-05-17 23:22:46 +0200

    async: variable declare

---
+++ async/index.js
@@ -0,0 +1,12 @@
+let stocks = {
+    Fruits: ["strawberry", "grapes", "banana", "apple"],
+    liquid: ["water", "ice"],
+    holder: ["cone", "cup", "stick"],
+    toppings: ["chocolate", "peanuts"]
+}
+
+let is_shop_open = true;
+
+let order = (time, work) => {
+
+}

e97c1aa7008f11ba38873e36be40de88994f1178
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-05-18 16:14:56 +0200

    async: await keyword

--- async/index2.js
+++ async/index2.js
@@ -7,17 +7,31 @@ let stocks = {
 
 let is_shop_open = true;
 
-async function order() {
-    try {
-        await abc;
-    } catch (error) {
-        console.log("abc doesn't exist", error);
-    }
+let toppings_choice = () => {
+    
+    return new Promise((resolve, reject) => {
+        
+        setTimeout(() => {
+            resolve(
+                console.log("Which toppings would do you love?")
+            )
+        }, 3000);
+    })
+};
 
-    finally {
-        console.log("runs codes anyways")
-    }
+async function kitchen() {
+    console.log(" A ");
+    console.log(" B ");
+    console.log(" C ");
+
+    await toppings_choice();
+
+    console.log(" D ");
+    console.log(" E ");
 }
 
+kitchen();
 
-order()
+console.log("Doing the dishes");
+console.log("Cleaning the table");
+console.log("Taking other orders");

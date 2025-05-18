ee71fa8ced93087adcb0af32e0e8b016ee2d6c1f
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-05-18 15:59:47 +0200

    async: test handlers

---
+++ async/index2.js
@@ -0,0 +1,23 @@
+let stocks = {
+    Fruits: ["strawberry", "grapes", "banana", "apple"],
+    liquid: ["water", "ice"],
+    holder: ["cone", "cup", "stick"],
+    toppings: ["chocolate", "peanuts"]
+}
+
+let is_shop_open = true;
+
+async function order() {
+    try {
+        await abc;
+    } catch (error) {
+        console.log("abc doesn't exist", error);
+    }
+
+    finally {
+        console.log("runs codes anyways")
+    }
+}
+
+
+order()

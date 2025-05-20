27cb8e48d554019d82240cc5d8c0f0f7bc3aa893
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-05-20 16:57:36 +0200

    async: Fetching

---
+++ async/fetch/main.js
@@ -0,0 +1,4 @@
+// Fetch: Function used for making HTTP requests to fetch resources.(JSON style data,   images, files)
+        // Simplifies asynchronous data fetching in JavaScript and used for interacting with APIs to retrieve and send data asynchronously over the web. fetch("url")
+
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu");

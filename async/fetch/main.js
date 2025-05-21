41eea5a78f3aec6d0a8ca4a4a44a16959199aeec
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-05-21 15:16:18 +0200

    async: Promise version

--- async/fetch/main.js
+++ async/fetch/main.js
@@ -1,4 +1,13 @@
 // Fetch: Function used for making HTTP requests to fetch resources.(JSON style data,   images, files)
         // Simplifies asynchronous data fetching in JavaScript and used for interacting with APIs to retrieve and send data asynchronously over the web. fetch("url")
 
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+fetch("https://pokeapi.co/api/v2/pokemon/spongebob")
+        .then(response => {
+                if(!response.ok) {
+                        throw new Error("Could not fetch the resources");
+                }
+
+                return response.json();
+        })
+        .then(data => console.log(data))
+        .catch(error => console.error(error));

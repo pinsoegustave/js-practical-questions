70b62ad14e596890beab8ef1eeef7c6fcdf3ea96
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-05-31 23:35:47 +0200

    async-learn: Add data to HTML

--- async/main.js
+++ async/main.js
@@ -9,6 +9,18 @@ fetch(apiUrl)
         return response.json();
     })
     .then(userData => {
+        const head = document.getElementById('user-details');
+        // userData.map(element => {
+            const newList = document.createElement('li');
+            newList.innerHTML = `<li>Person_id: ${userData.id}</li><br/>
+            <li>Name: ${userData.name}</li><br/>
+            <li>Userame: ${userData.username}</li><br/>
+            <li>Email: ${userData.email}</li><br/>
+            <li>Adress(City): ${userData.address.city}</li><br/>
+            <li>Address(Street): ${userData.address.street}</li><br/>`
+            head.appendChild(newList);
+        // });
+
         console.log('User Data:', userData);
     }) 
     .catch(error => {

1855b68513dccbfc70b7735e2dbdf81a6b1fc728
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-05-31 23:55:54 +0200

    async-learn: send posts

--- async/send-form/main.js
+++ async/send-form/main.js
@@ -1,10 +1,17 @@
-const apiUrl = 'https://jsonplaceholder.typicode.com/users'
+const apiUrl = 'https://jsonplaceholder.typicode.com/posts'
+
 
 const formData = {
-    username: 'john Doe',
-    email: 'johnlambo@gmail.com',
-    password: 'securePassword!@#32'
-};
+    userId: 10,
+    id: 101,
+    title: "My new post varies by the football matches",
+    body: "jsdflsdfsdojglkjfuhfosinjbkuiheionfiue0wrikmnvkbiurperfnkdjsfnifdsnjfkdsnfiudslfhskj"
+}
+// const formData = {
+//     username: 'john Doe',
+//     email: 'johnlambo@gmail.com',
+//     password: 'securePassword!@#32'
+// };
 
 fetch(apiUrl, {
     method: 'POST',

d2e7ac2b177931dfd39aac6092070421b705aedc
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-05-31 23:19:05 +0200

    async-learn: fetch one user

--- async/main.js
+++ async/main.js
@@ -1,4 +1,16 @@
-fetch('https://api.example.com/data')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+const apiUrl = 'https://jsonplaceholder.typicode.com/users/1';
+
+fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not good.')
+        }
+
+        return response.json();
+    })
+    .then(userData => {
+        console.log('User Data:', userData);
+    }) 
+    .catch(error => {
+        console.error('Error: ', error)
+    });

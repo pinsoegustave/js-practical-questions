cecc79350605bb2ab2872cafdbb04dde5b62130a
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-06-03 23:25:19 +0200

    async: digiocean

--- async/digOcean/authors.js
+++ async/digOcean/authors.js
@@ -4,3 +4,28 @@ const list = document.createDocumentFragment();
 const url = 'https://jsonplaceholder.typicode.com/users';
 
 fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        let authors = data;
+
+        console.log(data);
+
+        authors.map(function(author) {
+            let li = document.createElement('li');
+            let name = document.createElement('h2');
+            let email = document.createElement('span');
+
+            name.innerHTML = `${author.name}`;
+            email.innerHTML = `${author.email}`;
+
+            li.appendChild(name);
+            li.appendChild(email);
+            list.appendChild(li);
+        })
+    })
+    .catch((error) => {
+        console.error(error);
+    })
+ul.appendChild(list);

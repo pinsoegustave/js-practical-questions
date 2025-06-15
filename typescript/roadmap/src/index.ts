a6642cc45710459ab5be9553eeec9050f1818a2e
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-06-15 16:29:19 +0200

    ts: run class

---
+++ typescript/roadmap/src/index.ts
@@ -0,0 +1,15 @@
+class Car {
+  make: string;
+  model: string;
+  year: number;
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  drive() {
+    console.log(`Driving my ${this.year} ${this.make} ${this.model}`);
+  }
+}

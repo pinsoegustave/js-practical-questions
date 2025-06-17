3f819746eed06b0c2beba210743d00c7a0f877f8
Pinsoe Gustave <ntwalimudasubirag@gmail.com>
2025-06-17 16:56:35 +0200

    ts-learn: enum type

--- typescript/hytype/src/index.ts
+++ typescript/hytype/src/index.ts
@@ -1,6 +1,17 @@
-let numbers: number[] = [];
-numbers[0] = 1
-numbers[1] = 2
-numbers[2] = 3
 
-numbers.forEach(n => n.toFixed)
+
+// PascalCase
+const enum Size {
+    Small = 109,
+    Medium,
+    Large
+}
+let mySize: Size = Size.Medium
+console.log(mySize)
+
+// let numbers: number[] = [];
+// numbers[0] = 1
+// numbers[1] = 2
+// numbers[2] = 3
+
+// numbers.forEach(n => n.toFixed)

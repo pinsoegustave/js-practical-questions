79fa01849f5081f063b98a91099f8af754d0d058
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-07-18 16:03:21 +0200

    react-learn: state as a snapshot

--- react-learning/react-dev/src/main.jsx
+++ react-learning/react-dev/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Sculptures from './components/Sculptures.jsx'
+import Form from './components/StateAsA.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
   //   <App />
   // </StrictMode>,
-  <Sculptures />
+  // <Sculptures />
+  <Form />
 )

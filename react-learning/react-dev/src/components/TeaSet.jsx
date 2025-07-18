8679de8f7e196683a2f5438856c8b4cd68e49d7f
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-07-18 13:23:11 +0200

    react-learn: pure function by passing props

--- react-learning/react-dev/src/components/TeaSet.jsx
+++ react-learning/react-dev/src/components/TeaSet.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 
 let guest = 0
 
-function Cup()  {
-    guest = guest + 1;
+function Cup({ guest })  {
+    
     return <h2>Tea cup for guest #{guest}</h2>
 }
 // function to set the type of tea in the kitchen.
 export default function TeaSet() {
   return (
     <div>
-        <Cup />
-        <Cup />
-        <Cup />
-        <Cup />
-        <Cup />
+        <Cup guest={1} />
+        <Cup guest={2} />
+        <Cup guest={3} />
+        <Cup guest={5} />
+        <Cup guest={9} />
     </div>
   )
 }
